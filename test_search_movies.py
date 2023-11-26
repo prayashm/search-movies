2f63a601d21ad5e74cc620f73a915e3189a6c6ec
Prayash Mohapatra <gh@prayashm.com>
2023-11-26 12:42:53 +0530

    test: Case-insensitive

--- test_search_movies.py
+++ test_search_movies.py
@@ -7,12 +7,14 @@ class TestSearchMovies(unittest.TestCase):
     def setUp(self):
         self.filename = 'top250.json'
         self.index = make_index(self.filename)
+
     def test_search_movies(self):
         test_cases = [
             ('morgan', ['Unforgiven', 'The Wizard of Oz', 'The Shawshank Redemption', 'Se7en', 'Million Dollar Baby']),
             (' morgan ', ['Unforgiven', 'The Wizard of Oz', 'The Shawshank Redemption', 'Se7en', 'Million Dollar Baby']),
             ('freeman', ['Unforgiven', 'The Shawshank Redemption', 'Se7en', 'Raiders of the Lost Ark', 'Million Dollar Baby']),
             ('morgan freeman',['Unforgiven', 'The Shawshank Redemption', 'Se7en', 'Million Dollar Baby']),
+            ('MorGan FrEeMan', ['Unforgiven', 'The Shawshank Redemption', 'Se7en', 'Million Dollar Baby']),
             ('asdfasdfasdfasdf', [])
         ]
 
